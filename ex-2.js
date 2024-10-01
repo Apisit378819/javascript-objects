dc7fa0e1b1f7636bdadf28ab3498377efcc0454e
Tonkla <Apisit378819@gmail.com>
2024-10-01 13:15:16 +0700

    feat:add ex1-ex3

--- ex-2.js
+++ ex-2.js
@@ -5,6 +5,14 @@ const product = {
   price: 4000,
   ratings: 2.5,
   photo: null,
+  code: "IT0101010",
 };
 let socialMediaShare = "facebookShare";
 // Start code here
+product.ratings = 2.5
+product.photo = "https://i01.appmifile.com/webfile/globalimg/products/pc/mi-air-purifier-3H/replace_03.jpg"
+product.price = 6000
+delete product.ratings
+product[socialMediaShare] = 45.5
+
+console.log(product)
